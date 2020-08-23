e8b9c1f4c5cfbc950fc3df0670fbc409a37c0fb5
rowlet <tcrammy@gmail.com>
2020-08-23 10:35:00 +0800

    Format module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,18 @@
 module Sona {
 
-    // Exports
+    // ============================================================================================
+    // EXPORTS
+    // ============================================================================================
+
     exports main.java;
     exports main.java.presentation.model.structure.note;
     exports main.java.data.dto;
     exports main.java.data.dto.element;
     exports main.java.data.dto.enums;
+
+    // ============================================================================================
+    // DEPENDENCIES
+    // ============================================================================================
 
     // JavaFX
     requires javafx.controls;
