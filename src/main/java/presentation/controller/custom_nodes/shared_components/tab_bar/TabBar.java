2803ecce7162c4cd43db6ab16d36f12e44fde61f
rowlet <tcrammy@gmail.com>
2020-09-24 11:10:43 +0800

    Create tab bar

---
+++ src/main/java/presentation/controller/custom_nodes/shared_components/tab_bar/TabBar.java
@@ -0,0 +1,276 @@
+/*
+ * Sona is an educational SRS application written in JavaFX.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main.java.presentation.controller.custom_nodes.shared_components.tab_bar;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.collections.*;
+import javafx.fxml.FXML;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
+import main.java.presentation.controller.custom_nodes.main.rail_destinations.decks.deck_info_pane.tabs.DeckInfoTab;
+import main.java.presentation.controller.utils.DoubleFxUtils;
+import main.java.presentation.controller.utils.FxUtils;
+import main.java.presentation.controller.utils.color.ColorFxUtils;
+import main.java.presentation.controller.utils.color.provider.ColorProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO: 24/09/2020 Fix starting animation where indicator starts out at full size.
+public class TabBar extends AnchorPane {
+
+    // INSTANCE VARIABLES
+
+    private final ObservableMap<DeckInfoTab, TabItem> tabItems = FXCollections.observableHashMap();
+
+    private final Map<DeckInfoTab, Integer> tabItemIndices = new HashMap<>();
+
+
+    // JAVAFX PROPERTIES (Public Accessors / Mutators)
+
+    private final ObjectProperty<DeckInfoTab> activeTab = new SimpleObjectProperty<>();
+
+
+    // JAVAFX PROPERTIES (Style (Attributive))
+
+    private final StringProperty indicatorBackgroundColorStyle = new SimpleStringProperty();
+
+    // This property needs to be a field to prevent garbage collection! Do not listen to IntelliJ
+    // here.
+    private DoubleProperty desiredLeftAnchor = new SimpleDoubleProperty();
+    private DoubleProperty desiredRightAnchor = new SimpleDoubleProperty();
+
+    // JAVAFX PROPERTIES (Style (Nodal))
+
+    private final StringProperty indicatorStyle = new SimpleStringProperty();
+
+
+    // FXML NODES
+
+    @FXML
+    private HBox itemBar;
+
+    @FXML
+    private Pane indicator;
+
+
+    // CONSTRUCTOR
+
+    public TabBar() {
+        initializeFxml();
+
+        initializeItemBarBehavior();
+        initializeActiveTabListener();
+        initializeIndicatorBehavior();
+
+        initializeAttributiveStyleBindings();
+        initializeNodalStyleBindings();
+    }
+
+
+    // INITIALIZERS (FXML)
+
+    private void initializeFxml() {
+        final String RESOURCE_PATH = "/view/fxml/custom_nodes/shared_components/tab_bar/tab_bar"
+                                     + ".fxml";
+
+        FxUtils.initializeFxml(this, RESOURCE_PATH);
+    }
+
+
+    // INITIALIZERS (Behavior)
+
+    private void initializeItemBarBehavior() {
+        tabItems.addListener((MapChangeListener<DeckInfoTab, TabItem>) change -> {
+            System.out.println(change);
+            if (change.wasAdded()) {
+                final TabItem TAB_ITEM = change.getValueAdded();
+                addTabItem(TAB_ITEM);
+            }
+        });
+    }
+
+    private void initializeTabItemBehavior(TabItem tabItem) {
+        tabItem.setOnAction(e -> {
+            activeTab.set(tabItem.getDeckInfoTab());
+        });
+    }
+
+    private void initializeActiveTabListener() {
+        activeTab.addListener((obs, oldTab, newTab) -> {
+            if (oldTab != null)
+                tabItems.get(oldTab).setActivated(false);
+            tabItems.get(newTab).setActivated(true);
+        });
+    }
+
+
+    // INITIALIZERS (Behavior - Indicator)
+
+    private void initializeIndicatorBehavior() {
+        // There is no way to get the property of the anchor constraints of indicator.
+
+        final IntegerProperty POSITION = createPosition();
+        final IntegerProperty SIZE = createSize();
+
+        desiredLeftAnchor = createDesiredLeftAnchor(POSITION, SIZE);
+        desiredRightAnchor = createDesiredRightAnchor(POSITION, SIZE);
+
+        bindAnchors();
+    }
+
+    private IntegerProperty createPosition() {
+        final IntegerProperty POSITION = new SimpleIntegerProperty();
+        activeTabProperty().addListener((obs, oldTab, newTab) -> {
+            POSITION.set(tabItemIndices.get(newTab));
+        });
+        POSITION.set(0);
+        return POSITION;
+    }
+
+    private IntegerProperty createSize() {
+        final IntegerProperty SIZE = new SimpleIntegerProperty();
+        tabItems.addListener((MapChangeListener<DeckInfoTab, TabItem>) change -> {
+            SIZE.set(tabItems.size());
+        });
+        SIZE.set(1);
+        return SIZE;
+    }
+
+    private DoubleProperty createDesiredLeftAnchor(IntegerProperty position, IntegerProperty size) {
+        final DoubleProperty DESIRED_LEFT_ANCHOR = new SimpleDoubleProperty();
+        DESIRED_LEFT_ANCHOR.set(0.0);
+        DESIRED_LEFT_ANCHOR.bind(position.multiply(widthProperty().divide(size)));
+        return DESIRED_LEFT_ANCHOR;
+    }
+
+    private DoubleProperty createDesiredRightAnchor(IntegerProperty position, IntegerProperty size) {
+        final DoubleProperty DESIRED_RIGHT_ANCHOR = new SimpleDoubleProperty();
+        DESIRED_RIGHT_ANCHOR.bind((size.subtract(position).subtract(1))
+                                          .multiply(widthProperty().divide(size)));
+        return DESIRED_RIGHT_ANCHOR;
+    }
+
+    private void bindAnchors() {
+        final Duration TRANSITION_DURATION = Duration.millis(150);
+
+        bindLeftAnchor(TRANSITION_DURATION);
+        bindRightAnchor(TRANSITION_DURATION);
+    }
+
+    private void bindLeftAnchor(Duration transitionDuration) {
+        final DoubleProperty LEFT_ANCHOR =
+                DoubleFxUtils.createDynamicDoubleProperty(desiredLeftAnchor,
+                                                          transitionDuration);
+
+        LEFT_ANCHOR.addListener((obs, oldAnchor, newAnchor) -> {
+            AnchorPane.setLeftAnchor(indicator, newAnchor.doubleValue());
+        });
+    }
+
+    private void bindRightAnchor(Duration transitionDuration) {
+        final DoubleProperty RIGHT_ANCHOR =
+                DoubleFxUtils.createDynamicDoubleProperty(desiredRightAnchor,
+                                                          transitionDuration);
+
+        RIGHT_ANCHOR.addListener((obs, oldAnchor, newAnchor) -> {
+            AnchorPane.setRightAnchor(indicator, newAnchor.doubleValue());
+        });
+    }
+
+
+    // INITIALIZERS (Adding Methods)
+
+    private void addTabItem(TabItem tabItem) {
+        final AnchorPane TAB_ITEM_PANE = new AnchorPane(tabItem);
+        itemBar.getChildren().add(TAB_ITEM_PANE);
+
+        FxUtils.initializeAnchorPaneAnchors(tabItem);
+
+        initializeTabItemPaneDistribution(TAB_ITEM_PANE);
+        initializeTabItemBehavior(tabItem);
+    }
+
+    private void initializeTabItemPaneDistribution(AnchorPane tabItemPane) {
+        HBox.setHgrow(tabItemPane, Priority.ALWAYS);
+    }
+
+
+    // INITIALIZERS (Style Bindings (Called in constructor))
+
+    private void initializeAttributiveStyleBindings() {
+        initializeIndicatorBackgroundColorStyleBindings();
+    }
+
+    private void initializeNodalStyleBindings() {
+        initializeIndicatorStyleBinding();
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Attributive indicatorBackgroundColorStyle))
+
+    private void initializeIndicatorBackgroundColorStyleBindings() {
+        final StringProperty BACKGROUND_COLOR_STRING =
+                ColorFxUtils.createStaticStringProperty(ColorProvider.tabBarIndicatorColorProperty());
+
+        indicatorBackgroundColorStyle.bind(Bindings.concat("-fx-background-color: ",
+                                                           BACKGROUND_COLOR_STRING,
+                                                           ";"));
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Nodal))
+
+    private void initializeIndicatorStyleBinding() {
+        indicatorStyle.bind(indicatorBackgroundColorStyle);
+        indicator.styleProperty().bind(indicatorStyle);
+    }
+
+
+    // PUBLIC METHODS
+
+    public void addTab(DeckInfoTab deckInfoTab, String text, char icon) {
+        final TabItem TAB_ITEM = new TabItem(deckInfoTab, text, icon);
+
+        tabItems.put(deckInfoTab, TAB_ITEM);
+        tabItemIndices.put(deckInfoTab, tabItemIndices.size());
+    }
+
+
+    // ACCESSORS
+
+    public DeckInfoTab getActiveTab() {
+        return activeTab.get();
+    }
+
+
+    // PROPERTY ACCESSORS
+
+    public ObjectProperty<DeckInfoTab> activeTabProperty() {
+        return activeTab;
+    }
+
+
+    // MUTATORS
+
+    public void setActiveTab(DeckInfoTab activeTab) {
+        this.activeTab.set(activeTab);
+    }
+}
---
+++ src/main/java/presentation/controller/custom_nodes/shared_components/tab_bar/TabItem.java
@@ -0,0 +1,244 @@
+/*
+ * Sona is an educational SRS application written in JavaFX.
+ * Copyright (C) 2020 Cervon Wong
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main.java.presentation.controller.custom_nodes.shared_components.tab_bar;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+import main.java.presentation.controller.custom_nodes.main.rail_destinations.decks.deck_info_pane.tabs.DeckInfoTab;
+import main.java.presentation.controller.utils.FxUtils;
+import main.java.presentation.controller.utils.color.ColorFxUtils;
+import main.java.presentation.controller.utils.color.provider.ColorProvider;
+
+public class TabItem extends Button {
+
+    // INSTANCE VARIABLES
+
+    private final DeckInfoTab deckInfoTab;
+
+    private final String text;
+
+    private final char icon;
+
+
+    // CONSTANTS (Style)
+
+
+    // JAVAFX PROPERTIES (Public Accessors / Mutators)
+
+    private final BooleanProperty isActivated = new SimpleBooleanProperty();
+
+
+    // JAVAFX PROPERTIES (Style (Attributive))
+
+    private final StringProperty backgroundColorStyle = new SimpleStringProperty();
+
+    private final StringProperty textFillStyle = new SimpleStringProperty();
+
+
+    // JAVAFX PROPERTIES (Style (Nodal))
+
+    private final StringProperty thisStyle = new SimpleStringProperty();
+
+    private final StringProperty iconLabelStyle = new SimpleStringProperty();
+
+
+    // FXML NODES
+
+    @FXML
+    private Label iconLabel;
+
+
+    // CONSTRUCTOR
+
+    public TabItem(DeckInfoTab deckInfoTab, String text, char icon) {
+        this.deckInfoTab = deckInfoTab;
+        this.text = text;
+        this.icon = icon;
+
+        initializeFxml();
+
+        initializeTexts();
+
+        initializeAttributiveStyleBindings();
+        initializeNodalStyleBindings();
+    }
+
+
+    // INITIALIZERS (FXML)
+
+    private void initializeFxml() {
+        final String RESOURCE_PATH = "/view/fxml/custom_nodes/shared_components/tab_bar/tab_item"
+                                     + ".fxml";
+
+        FxUtils.initializeFxml(this, RESOURCE_PATH);
+    }
+
+
+    // INITIALIZERS (Text)
+
+    private void initializeTexts() {
+        initializeThisText();
+        initializeIconLabelText();
+    }
+
+    private void initializeThisText() {
+        this.setText(text);
+    }
+
+    private void initializeIconLabelText() {
+        iconLabel.setText(String.valueOf(icon));
+    }
+
+
+    // INITIALIZERS (Style Bindings (Called in constructor))
+
+    private void initializeAttributiveStyleBindings() {
+        initializeBackgroundColorStyleBindings();
+        initializeTextFillStyleBindings();
+    }
+
+    private void initializeNodalStyleBindings() {
+        initializeThisStyleBinding();
+        initializeIconLabelStyleBinding();
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Attributive backgroundColorStyle))
+
+    private void initializeBackgroundColorStyleBindings() {
+        final Duration TRANSITION_DURATION = Duration.millis(150);
+
+        final ObjectProperty<Color> DESIRED_BACKGROUND_COLOR = createDesiredBackgroundColor();
+
+        final StringProperty BACKGROUND_COLOR_STRING =
+                ColorFxUtils.createDynamicStringProperty(DESIRED_BACKGROUND_COLOR,
+                                                         TRANSITION_DURATION);
+
+        backgroundColorStyle.bind(Bindings.concat("-fx-background-color: ",
+                                                  BACKGROUND_COLOR_STRING,
+                                                  ";"));
+    }
+
+    private ObjectProperty<Color> createDesiredBackgroundColor() {
+        final ObjectProperty<Color> DESIRED_BACKGROUND_COLOR = new SimpleObjectProperty<>();
+
+        final ObjectProperty<Color> ENABLED_BACKGROUND_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.enabledTabItemBackgroundColorProperty());
+
+        final ObjectProperty<Color> HOVER_BACKGROUND_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.hoverTabItemBackgroundColorProperty());
+
+        final ObjectProperty<Color> PRESSED_BACKGROUND_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.pressedTabItemBackgroundColorProperty());
+
+        hoverProperty()
+                .addListener((obs, oldValue, newValue)
+                                     -> DESIRED_BACKGROUND_COLOR.bind(newValue
+                                                                      ? HOVER_BACKGROUND_COLOR
+                                                                      : ENABLED_BACKGROUND_COLOR));
+
+        pressedProperty()
+                .addListener((obs, oldValue, newValue)
+                                     -> DESIRED_BACKGROUND_COLOR.bind(newValue
+                                                                      ? PRESSED_BACKGROUND_COLOR
+                                                                      : isHover()
+                                                                        ? HOVER_BACKGROUND_COLOR
+                                                                        :
+                                                                        ENABLED_BACKGROUND_COLOR));
+
+        DESIRED_BACKGROUND_COLOR.bind(ENABLED_BACKGROUND_COLOR);
+
+        return DESIRED_BACKGROUND_COLOR;
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Attributive textFillStyle))
+
+    private void initializeTextFillStyleBindings() {
+        final Duration TRANSITION_DURATION = Duration.millis(150);
+
+        final ObjectProperty<Color> DESIRED_TEXT_COLOR = createDesiredTextColor();
+
+        final StringProperty TEXT_FILL_STRING =
+                ColorFxUtils.createDynamicStringProperty(DESIRED_TEXT_COLOR, TRANSITION_DURATION);
+
+        textFillStyle.bind(Bindings.concat("-fx-text-fill: ", TEXT_FILL_STRING, ";"));
+    }
+
+    private ObjectProperty<Color> createDesiredTextColor() {
+        final ObjectProperty<Color> DESIRED_TEXT_COLOR = new SimpleObjectProperty<>();
+
+        final ObjectProperty<Color> ENABLED_TEXT_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.mediumEmphasisTextColorProperty());
+
+        final ObjectProperty<Color> ACTIVATED_TEXT_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.activatedTabItemTextColorProperty());
+
+        isActivated.addListener((obs, oldValue, newValue)
+                                        -> DESIRED_TEXT_COLOR.bind(newValue
+                                                                   ? ACTIVATED_TEXT_COLOR
+                                                                   : ENABLED_TEXT_COLOR));
+        DESIRED_TEXT_COLOR.bind(ENABLED_TEXT_COLOR);
+
+        return DESIRED_TEXT_COLOR;
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Nodal))
+
+    private void initializeThisStyleBinding() {
+        thisStyle.bind(Bindings.concat(backgroundColorStyle, textFillStyle));
+        styleProperty().bind(thisStyle);
+    }
+
+    private void initializeIconLabelStyleBinding() {
+        iconLabelStyle.bind(textFillStyle);
+        iconLabel.styleProperty().bind(iconLabelStyle);
+    }
+
+
+    // ACCESSORS
+
+    public DeckInfoTab getDeckInfoTab() {
+        return deckInfoTab;
+    }
+
+    public boolean isActivated() {
+        return isActivated.get();
+    }
+
+
+    // PROPERTY ACCESSORS
+
+    public BooleanProperty isActivatedProperty() {
+        return isActivated;
+    }
+
+
+    // MUTATORS
+
+    public void setActivated(boolean isActivated) {
+        this.isActivated.set(isActivated);
+    }
+}
