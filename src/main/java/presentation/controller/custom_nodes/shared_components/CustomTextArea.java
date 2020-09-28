326da0b5c7dd77bc45729297d60c36eb738abb94
rowlet <tcrammy@gmail.com>
2020-09-29 07:59:23 +0800

    Style custom text area

--- src/main/java/presentation/controller/custom_nodes/shared_components/CustomTextArea.java
+++ src/main/java/presentation/controller/custom_nodes/shared_components/CustomTextArea.java
@@ -18,14 +18,26 @@
 
 package main.java.presentation.controller.custom_nodes.shared_components;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
 import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+import main.java.presentation.controller.utils.DoubleFxUtils;
 import main.java.presentation.controller.utils.FxUtils;
+import main.java.presentation.controller.utils.color.ColorFxUtils;
+import main.java.presentation.controller.utils.color.provider.ColorProvider;
 
 // TODO: 28/09/2020 INCOMPLETE STYLING!!!!!!!
 // TODO: 28/09/2020 In the future, use GenericStyledArea.
 public class CustomTextArea extends TextArea {
+
+    // CONSTANTS (Style)
+
+    private final static double ENABLED_ACCENT_HEIGHT = 1.0;
+
+    private final static double HOVER_ACCENT_HEIGHT = 2.0;
+
 
     // JAVAFX PROPERTIES (Style (Attributive))
 
@@ -60,15 +72,97 @@ public class CustomTextArea extends TextArea {
 
     // INITIALIZERS (Style Bindings (Called in constructor))
 
-    private void initializeAttributiveStyleBindings() {}
+    private void initializeAttributiveStyleBindings() {
+        initializeBackgroundColorStyleBindings();
+        initializeBackgroundInsetsStyleBindings();
+    }
 
-    private void initializeNodalStyleBindings() {}
+    private void initializeNodalStyleBindings() {
+        initializeThisStyleBinding();
+    }
 
 
     // INITIALIZERS (Style Bindings (Internal - Attributive backgroundColorStyle)
 
+    private void initializeBackgroundColorStyleBindings() {
+        final Duration TRANSITION_DURATION = Duration.millis(75);
+
+        final StringProperty BASE_BACKGROUND_COLOR_STRING =
+                ColorFxUtils.createStaticStringProperty(ColorProvider.backgroundColorProperty());
+        final ObjectProperty<Color> DESIRED_ACCENT_BACKGROUND_COLOR =
+                createDesiredAccentBackgroundColor();
+
+        final StringProperty ACCENT_BACKGROUND_COLOR_STRING =
+                ColorFxUtils.createDynamicStringProperty(DESIRED_ACCENT_BACKGROUND_COLOR,
+                                                         TRANSITION_DURATION);
+
+        backgroundColorStyle.bind(Bindings.concat("-fx-background-color: ",
+                                                  ACCENT_BACKGROUND_COLOR_STRING,
+                                                  ", ",
+                                                  BASE_BACKGROUND_COLOR_STRING,
+                                                  ";"));
+    }
+
+    private ObjectProperty<Color> createDesiredAccentBackgroundColor() {
+        final ObjectProperty<Color> DESIRED_ACCENT_BACKGROUND_COLOR = new SimpleObjectProperty<>();
+
+        final ObjectProperty<Color> ENABLED_ACCENT_BACKGROUND_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.enabledTextFieldAccentColorProperty());
+
+        final ObjectProperty<Color> HOVER_ACCENT_BACKGROUND_COLOR =
+                ColorFxUtils.createStaticColorProperty(ColorProvider.hoverTextFieldAccentColorProperty());
+
+        hoverProperty()
+                .addListener((obs, oldValue, newValue)
+                                     -> DESIRED_ACCENT_BACKGROUND_COLOR.bind(newValue
+                                                                             ?
+                                                                             HOVER_ACCENT_BACKGROUND_COLOR
+                                                                             :
+                                                                             ENABLED_ACCENT_BACKGROUND_COLOR));
+        DESIRED_ACCENT_BACKGROUND_COLOR.bind(ENABLED_ACCENT_BACKGROUND_COLOR);
+
+        return DESIRED_ACCENT_BACKGROUND_COLOR;
+    }
+
+
+    // INITIALIZERS (Style Bindings (Internal - Attributive backgroundInsetsStyle)
+
+    private void initializeBackgroundInsetsStyleBindings() {
+        final Duration TRANSITION_DURATION = Duration.millis(75);
+
+        final DoubleProperty DESIRED_PARAMETRIC_BACKGROUND_INSET =
+                createDesiredParametricBackgroundInset();
+
+        final DoubleProperty PARAMETRIC_BACKGROUND_INSET =
+                DoubleFxUtils.createDynamicDoubleProperty(
+                DESIRED_PARAMETRIC_BACKGROUND_INSET,
+                TRANSITION_DURATION);
+
+        backgroundInsetsStyle.bind(Bindings.concat("-fx-background-insets: 0, 0 0 ",
+                                                   PARAMETRIC_BACKGROUND_INSET,
+                                                   " 0;"));
+    }
+
+    private DoubleProperty createDesiredParametricBackgroundInset() {
+        DoubleProperty DESIRED_PARAMETRIC_BACKGROUND_INSET = new SimpleDoubleProperty();
+
+        hoverProperty()
+                .addListener((obs, oldValue, newValue)
+                                     -> DESIRED_PARAMETRIC_BACKGROUND_INSET.set(newValue
+                                                                                ?
+                                                                                HOVER_ACCENT_HEIGHT
+                                                                                :
+                                                                                ENABLED_ACCENT_HEIGHT));
+        DESIRED_PARAMETRIC_BACKGROUND_INSET.set(ENABLED_ACCENT_HEIGHT);
+
+        return DESIRED_PARAMETRIC_BACKGROUND_INSET;
+    }
+
 
     // INITIALIZERS (Style Bindings (Internal - Nodal))
 
-
+    private void initializeThisStyleBinding() {
+        thisStyle.bind(Bindings.concat(backgroundColorStyle, backgroundInsetsStyle));
+        styleProperty().bind(thisStyle);
+    }
 }
