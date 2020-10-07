26695ed8a1fd57fd32b0c205fe995bb29e862876
rowlet <tcrammy@gmail.com>
2020-10-07 09:38:47 +0800

    Add shortcut key to toggle theme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,34 +19,57 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import main.java.presentation.controller.custom_nodes.main.MainPane;
-
-import java.util.Locale;
+import main.java.presentation.controller.enums.Theme;
+import main.java.presentation.controller.utils.color.provider.ColorProvider;
 
 public class Main extends Application {
 
+    // VARIABLES
+
+    private static final ObjectProperty<KeyEvent> keyEvent = new SimpleObjectProperty<>();
+
+
+    // START METHODS
+
     @Override
     public void start(Stage primaryStage) {
-        Locale.setDefault(Locale.FRANCE);
+//        Locale.setDefault(Locale.FRANCE);
 
         final MainPane ROOT = new MainPane();
+        final Scene SCENE = new Scene(ROOT, 0, 0, false, SceneAntialiasing.BALANCED);
 
         primaryStage.setTitle("Sona (v.DEV)");
-        primaryStage.setScene(new Scene(ROOT, 0, 0, false, SceneAntialiasing.BALANCED));
+        primaryStage.setScene(SCENE);
         primaryStage.setMaximized(true);
 
         // TODO: 10/09/2020 Remove hard code.
         primaryStage.setMinHeight(400);
         primaryStage.setMinWidth(600);
 
+        initializeKeyEventBinding(SCENE);
+        initializeThemeToggleBehavior(); // TODO: 06/10/2020 Migrate this to RootPane.java in the
+        // future.
+
         setStageIcon(primaryStage);
 
         primaryStage.show();
     }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    // HELPER METHODS
 
     private void setStageIcon(Stage primaryStage) {
         final String RESOURCE_PATH = "/images/stage_icon/filled_440.png";
@@ -54,8 +77,26 @@ public class Main extends Application {
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream(RESOURCE_PATH)));
     }
 
+    private void initializeKeyEventBinding(Scene scene) {
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent::set);
+    }
 
-    public static void main(String[] args) {
-        launch(args);
+    private void initializeThemeToggleBehavior() {
+        keyEvent.addListener((obs, oldValue, newValue) -> {
+            if (newValue.isControlDown()
+                && newValue.isAltDown()
+                && newValue.getCode().getChar().equals("T")) {
+                ColorProvider.setTheme(ColorProvider.getTheme() == Theme.LIGHT
+                                       ? Theme.DARK
+                                       : Theme.LIGHT);
+            }
+        });
+    }
+
+
+    // ACCESSORS
+
+    public static ObjectProperty<KeyEvent> keyEventProperty() {
+        return keyEvent;
     }
 }
